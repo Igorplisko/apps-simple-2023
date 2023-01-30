fd03f5c0a820eb33171c5c2cdc0d644756011683
Igorplisko <igorplisko95@gmail.com>
2023-01-31 00:02:50 +0200

    31-01-2023

--- react-material-layout/src/App.js
+++ react-material-layout/src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
-import { IconButton, Container, Toolbar, AppBar } from '@mui/material';
-
+import { IconButton, Container, Toolbar, AppBar, Typography } from '@mui/material';
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
 function App() {
   return (
-    <AppBar position="fixed">
+    <AppBar position='fixed'>
       <Container fixed>
         <Toolbar>
-       <IconButton>
-
-       </IconButton>
+          <IconButton edge='start' color='inherit' aria-label='menu'>
+            <MenuSharpIcon/>
+            <Typography>Igor Plisko</Typography>
+          </IconButton>
         </Toolbar>
       </Container>
     </AppBar>
+    
   );
 }
 
+//9:28
 export default App;
